f719d79dada7456be0a95c308d6f7ed3643ee7f3
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-04-28 18:15:20 -0700

    Updated index.js to remove taglines prepended to top of vtt file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var encoding = require('./encoding');
-var reader = require('./reader');
+var encoding = require("./encoding");
+var reader = require("./reader");
 
 function convert(buf, defaultCodepageOverride, next) {
   // Handle defaultCodepageOverride being optional.
@@ -8,37 +8,33 @@ function convert(buf, defaultCodepageOverride, next) {
     defaultCodepageOverride = undefined;
   }
 
-  var outputLines = [
-  'WEBVTT',
-  '',
-  'NOTE Converted from .srt via srt2vtt: https://github.com/deestan/srt2vtt'
-  ];
+  var outputLines = [];
 
   var r = reader(buf);
-  r.on('error', next);
-  r.on('subtitle', function(subtitle) {
-    outputLines.push('');
+  r.on("error", next);
+  r.on("subtitle", function(subtitle) {
+    outputLines.push("");
     outputLines.push(subtitle.id);
     outputLines.push(subtitle.cue);
     outputLines = outputLines.concat(subtitle.lines);
   });
-  r.on('end', function() {
+  r.on("end", function() {
     var outLen = 0;
-    outputLines.forEach(function(line) { outLen += line.length + 1; });
-    var output = new Buffer(outLen);
-    for (var i = 0, pos = 0;
-      i < outputLines.length; i++) {
-        var lineBuf = outputLines[i];
-        if (!Buffer.isBuffer(lineBuf))
-          lineBuf = new Buffer(lineBuf);
-          lineBuf.copy(output, pos);
-          pos += outputLines[i].length;
-          output[pos] = 10;
-          pos += 1;
-        }
-        next(null, output);
+    outputLines.forEach(function(line) {
+      outLen += line.length + 1;
     });
-    r.start();
+    var output = new Buffer(outLen);
+    for (var i = 0, pos = 0; i < outputLines.length; i++) {
+      var lineBuf = outputLines[i];
+      if (!Buffer.isBuffer(lineBuf)) lineBuf = new Buffer(lineBuf);
+      lineBuf.copy(output, pos);
+      pos += outputLines[i].length;
+      output[pos] = 10;
+      pos += 1;
+    }
+    next(null, output);
+  });
+  r.start();
 }
 
 module.exports = function srt2vtt(buf, defaultCodepageOverride, next) {
